2d7a9e490a273daffe6d203aa7ccfacf250c6000
Juani Schreter <juaniischreter@gmail.com>
2023-11-11 00:57:47 -0300

    I add files of login and register

--- src/app.js
+++ src/app.js
@@ -9,6 +9,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/home.html"));
 });
 
+app.get("/views/login.html", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get("/views/register.html", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+
 const port = process.env.PORT || 3030;
 app.listen(port, () => {
   console.log(`Servidor iniciado en http://localhost:${port}`);
